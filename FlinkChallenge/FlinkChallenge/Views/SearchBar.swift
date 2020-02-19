29f6079202ef43babdba6774f506459e3ce8f0f1
Fernando Martin Garcia Del Angel <martingarciadelangel@me.com>
2020-02-18 23:39:54 -0600

    Created a SearchBar controller
    Will add it to the main controller

---
+++ FlinkChallenge/FlinkChallenge/Views/SearchBar.swift
@@ -0,0 +1,40 @@
+//
+//  SearchBar.swift
+//  FlinkChallenge
+//
+//  Created by Fernando Martin Garcia Del Angel on 18/02/20.
+//  Copyright © 2020 Fernando Martin Garcia Del Angel. All rights reserved.
+//
+
+import SwiftUI
+
+struct SearchBar: UIViewRepresentable {
+    @Binding var text : String
+    
+    class Coordinator : NSObject, UISearchBarDelegate {
+        @Binding var text : String
+        
+        init(text: Binding<String>) {
+            _text = text
+        }
+        
+        func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+            text = searchText
+        }
+    }
+    
+    func makeCoordinator() -> SearchBar.Coordinator {
+        return Coordinator(text: $text)
+    }
+    
+    func makeUIView(context: UIViewRepresentableContext<SearchBar>) -> UISearchBar {
+        let searchBar = UISearchBar(frame: .zero)
+        searchBar.delegate = context.coordinator
+        searchBar.searchBarStyle = .minimal
+        return searchBar
+    }
+    
+    func updateUIView(_ uiView: UISearchBar, context: UIViewRepresentableContext<SearchBar>) {
+        uiView.text = text
+    }
+}
