85324f4198366a64d502637b40762684c483c763
Fernando Martin Garcia Del Angel <martingarciadelangel@me.com>
2020-02-18 18:11:14 -0600

    Created Card View and centered it into the list
    TODO: Create a header

--- FlinkChallenge/FlinkChallenge/Views/CharacterFeedView.swift
+++ FlinkChallenge/FlinkChallenge/Views/CharacterFeedView.swift
@@ -12,22 +12,16 @@ struct CharacterFeedView: View {
     @ObservedObject var characterFeed = CharacterFeed()
     
     var body: some View {
-        List(characterFeed) { (character: APICharacter) in
-            CharacterListItemView(character: character)
-                .onAppear {
-                    self.characterFeed.loadMoreCharacters(currentItem: character)
-            }
-        }
-    }
-}
-
-struct CharacterListItemView : View {
-    var character : APICharacter
-    var body: some View {
-        HStack {
-            VStack(alignment: .leading) {
-                Text("\(character.name ?? "Gazorpazork")")
-                Text("\(character.gender ?? "None")").font(.subheadline)
+        VStack {
+            List(characterFeed) { (character: APICharacter) in
+                HStack{
+                    Spacer()
+                    Card(character: character).frame(width: 300, height: 300)
+                        .onAppear {
+                            self.characterFeed.loadMoreCharacters(currentItem: character)
+                    }
+                    Spacer()
+                }
             }
         }
     }
